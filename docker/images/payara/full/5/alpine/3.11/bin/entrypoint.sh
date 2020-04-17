efdf656fb942656cf6f48a7513bf0131c7c37d2c
icorrea <italiviocorrea@gmail.com>
2020-04-17 15:28:08 -0400

    Adicionado a image payara full

---
+++ docker/images/payara/full/5/alpine/3.11/bin/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Run init scripts (credits go to MySQL Docker entrypoint script)
+for f in ${SCRIPT_DIR}/init_* ${SCRIPT_DIR}/init.d/*; do
+      case "$f" in
+        *.sh)  echo "[Entrypoint] running $f"; . "$f" ;;
+        *)     echo "[Entrypoint] ignoring $f" ;;
+      esac
+      echo
+done
+
+exec ${SCRIPT_DIR}/startInForeground.sh $PAYARA_ARGS
