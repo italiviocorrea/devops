63b357636c3b174cb8012e80c9a94297ec5274d5
icorrea <italiviocorrea@gmail.com>
2021-07-14 18:05:14 -0400

    Ajustando o repositorio

--- docker/images/desktop app/jappmodeler/app/jappmodeler/jappmodeler.sh
+++ docker/images/desktop app/jappmodeler/app/jappmodeler/jappmodeler.sh
@@ -22,5 +22,7 @@ JAPPMODELER_CLASSPATH="${EXEDIR}/conf/":$JAPPMODELER_CLASSPATH
 
 JAPPMODELER_CLASSPATH="${EXEDIR}/./classes":$JAPPMODELER_CLASSPATH
 JAPPMODELER_HOME="${EXEDIR}/."
+export http_proxy=http://proxy.sgi.ms.gov.br:8081
+export https_proxy=http://proxy.sgi.ms.gov.br:8081
 
 java -classpath "$JAPPMODELER_CLASSPATH:$CLASSPATH" -D-server -D-Xms256m -D-Xmx1024 -D-XX:PermSize=32m -D-XX:MaxPermSize=512m -DjAppBuilder.home=$JAPPMODELER_HOME  org.jmycrudframework.jappmodeler.Main "$@"
--- docker/images/openproject/10/build.sh
+++ docker/images/openproject/10/build.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/bin/bash
 
 RELEASE_NAME=`cat release`
@@ -7,3 +8,13 @@ docker build -t $RELEASE_NAME .
 
 echo "Empurrando a imagem $RELEASE_NAME para o repositorio"
 docker push $RELEASE_NAME
+=======
+#!/bin/sh
+
+IMAGE_NAME=`cat release`
+echo "Criando a image : ${IMAGE_NAME}" && \ 
+ docker build -t $IMAGE_NAME . && \
+ echo "Empurrando a imagem para o repositório : ${IMAGE_NAME}"  && \
+ docker push $IMAGE_NAME && \
+ echo "Imagem criada e armazenada com sucesso!"
+>>>>>>> Ajuste no projeto jappmodeler, adição imagens nginx e openproject.
